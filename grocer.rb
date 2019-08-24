4184730c987933c1ae8bb173c01d986f3027f9f3
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2019-08-24 00:04:53 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,6 +1,24 @@
 def consolidate_cart(cart)
-  # code here
-end
+  new_hash = Hash.new
+  
+  cart.each do |item|
+  item_key = item.keys[0]
+   if new_hash.key?(item_key)
+    count = new_hash[item_key][:count]
+    new_hash[item_key][:count] = count + 1
+
+
+   else
+    item_value = item[item_key]
+    item_value[:count] = 1
+    new_hash[item_key] = item_value
+   end
+
+  end
+  return new_hash
+
+end  
+
 
 def apply_coupons(cart, coupons)
   # code here
